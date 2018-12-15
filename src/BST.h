fc0f6ffe023cdfc0e0acc46f124099fca63a5c5a
Tri hotboy <tridoan1998@gmail.com>
2018-12-15 12:47:59 -0800

    BST
    BST

---
+++ src/BST.h
@@ -0,0 +1,170 @@
+/*
+ * BST.h
+ *
+ *  Created on: Dec 13, 2018
+ *      Author: $$$$$$$$$$$$$$$$$$$$
+ */
+
+#ifndef BST_H_
+#define BST_H_
+
+#include <iostream>
+#include <string>
+
+class BST
+{
+private:
+	struct Node
+	{
+		int data;
+		Node* rightchild;
+		Node* leftchild;
+		Node(int d)
+		{
+			data = d;
+			rightchild = leftchild = NULL;
+		}
+	};
+	Node* root;
+
+    /**Private helper functions*/
+    void insertNode(Node* root, int data);
+    //private helper function for insert
+    //recursively inserts a value into the BST
+
+    void inOrderPrint(ostream& out, Node* root) const;
+    //private helper function for inOrderPrint
+    //recursively prints tree values in order from smallest to largest
+
+    void preOrderPrint(ostream& out, Node* root) const;
+    //private helper function for preOrderPrint
+    //recursively prints tree values in pre order
+
+    void postOrderPrint(ostream& out, Node* root) const;
+    //private helper function for postOrderPrint
+    //recursively prints tree values in post order
+
+    void copyNode(Node* copy);
+    //recursive helper function to the copy constructor
+
+    void freeNode(Node* root);
+    //private helper function for the destructor
+    //recursively frees the memory in the BST
+
+    bool searchNode(Node* root, int data) const;
+    //recursive helper function to search
+    //returns whether the value is found in the tree
+
+    bstdata minimum(Node* root) const;
+    //recursive helper function to minimum
+    //returns the minimum value in the tree
+
+    bstdata maximum(Node* root) const;
+    //recursive helper function to maximum
+    //returns the maximum value in the tree
+
+    Node* deleteNode(Node* root, int data);
+    //recursive helper function to remove
+    //removes data from the tree
+
+    void getSize(Node* root, int& size) const;
+    //recursive helper function to the size function
+    //calculates the size of the tree
+    //stores the result in size
+
+    int getHeight(Node* root) const;
+    //recursive helper function to the height function
+    //returns the height of the tree
+
+public:
+
+
+
+    /**constructors and destructor*/
+
+    BST();
+    //Instantiates a new BST
+
+    BST(const BST &bst);
+    //copy constructor
+
+    ~BST();
+    //deallocates the tree memory
+
+
+
+
+    /**access functions*/
+
+    bstdata minimum() const;
+    //returns the minimum value in the BST
+    //pre: !empty()
+
+    bstdata maximum() const;
+    //returns the maximum value in the BST
+    //pre: !empty()
+
+    bool isEmpty() const;
+    //determines whether the BST is empty
+
+    int getSize() const;
+    //returns the size of the tree
+
+    bstdata getRoot() const;
+    //returns the value stored at the root of the BST
+    //pre: !isEmpty()
+
+    int getHeight() const;
+    //returns the height of the tree
+
+    bool search(bstdata data) const;
+    //returns whether the data is found in the tree
+    //pre: !isEmpty()
+
+
+
+    /**manipulation procedures*/
+
+    //inserts new data into the BST
+
+    void remove(int data);
+    //removes the specified value from the tree
+    //pre: data is located in the tree
+    //pre: !isEmpty()
+
+    /**additional functions*/
+
+    void inOrderPrint(ostream& out) const;
+    //calls the inOrderPrint function to print out the values
+    //stored in the BST
+
+    void preOrderPrint(ostream& out) const;
+    //calls the reOrderPrint function to print out the values
+    //stored in the BST
+
+    void postOrderPrint(ostream& out) const;
+    //calls the postOrderPrint function to print out the values
+    //stored in the BST
+};
+
+BST::BST()
+{
+	root = NULL;
+}
+void BST::insertdata(Node* root, int data)
+{
+	if(root == NULL)
+	{
+		Node* N = new Node(data);
+		N->leftchild = NULL;
+		N->rightchild = NULL;
+	}
+	else if(root->Node(data) > data)
+	{
+		insertdata(root->leftchild, data);
+		Node* N = new Node(data);
+
+	}
+}
+
+#endif /* BST_H_ */
--- src/ReviewFor22C.cpp
+++ src/ReviewFor22C.cpp
@@ -1,10 +1,12 @@
 //============================================================================
-// Name        : ReviewFor22C.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+
+/*
+ *Creating a shopping list for CDs
+ *plus adding and deleteing CDs from the database.
+ */
+
 //============================================================================
+
 
 #include <iostream>
 #include <stdio.h>
@@ -13,16 +15,20 @@
 #include "song.h"
 #include <fstream>
 #include <sstream>
-
-//#include <iomanip>
+#include <iomanip>
+#include "BST.h"
 
 
 using namespace std;
 
 int main() {
 
+	/*
+	 * Count how many lines the txtfile has
+	 * so later we can create the amount of
+	 * objects needed.
+	 */
 	ifstream fileA;
-	//count lines of fileA
 	string line = "";
 	int count = 0;
 	fileA.open("txtfile.txt");
@@ -41,15 +47,15 @@ int main() {
 	}
 	fileA.close();
 
+
+	/*
+	 * Crate song objects using the count
+	 * variable from above.
+	 */
 	ifstream fileB;
-	//create song objects
 	Song song[count];
 	string datafromfile = "";
-	//set datas into song objects
-	//song[count].setsongname(title)
-
 	count = count/3;
-
 	int t = 0;
 		fileB.open("txtfile.txt");
 		if(fileB.is_open())
@@ -70,15 +76,12 @@ int main() {
 			cout << "Error opening fileA!";
 			exit(1);
 		}
-
 	for(int i = 0; i < count; i++)
 	{
 		cout << song[i].getsongname() << endl;
 		cout << song[i].getartist() << endl;
 		cout << song[i].getyear() << endl;
 	}
-
-
 	cout << endl;
 	Song* ptr[count];
 	for (int i = 0; i < count; i++)
@@ -86,7 +89,10 @@ int main() {
 		ptr[i] = &song[i];
 	}
 
-	//insert these objects into link list
+	/*
+	 * Insert these objects into link list.
+	 *
+	*/
 
 	Linklist A;
 	cout << endl << endl << endl;
@@ -96,6 +102,8 @@ int main() {
 	A.insertStart(2, ptr[0]);
 	A.insertEnd(3, ptr[1]);
 	A.getdatafromcurrentnode(2);
+
+
 	return 0;
 }
 
